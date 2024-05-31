2fdb4dfca1ae0a748f16caff2b05377ff688cb7e
Eason Huang <easonh887@gmail.com>
2024-05-30 21:46:24 -0400

    Added batch splitting for datasets

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -4,3 +4,12 @@ def load(dataset):
     if not os.path.exists(f"{dataset}.npz"):
         raise FileNotFoundError(f"Dataset \"{dataset}\" does not exist")
     return np.load(f"{dataset}.npz")
+
+def split_batches(batch_size, *datasets):
+    res = []
+    for ds in datasets:
+        num_batches = int(np.ceil(ds.shape[0] / batch_size))
+        res.append(np.array_split(ds, num_batches))
+    return res
+
+    
