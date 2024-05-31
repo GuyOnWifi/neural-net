2f165c83035448f0a3086a6a6657cd2207cc9f5a
Eason Huang <easonh887@gmail.com>
2024-05-31 13:36:04 -0400

    Changed some examples to utilize the network's new structure

--- examples/main.py
+++ examples/main.py
@@ -1,15 +1,15 @@
 import sys
-sys.path.append("../src")
+sys.path.append("../datasets")
+from dataset_loader import load, split_batches
 
-from datasets import load_dataset
-
-mnist = load_dataset('mnist')
-mnist = mnist.shuffle()
+data = load("mnist")
+train_data, train_ans, test_data, test_ans = data.values()
+train_data, train_ans = split_batches(256, train_data, train_ans)
 print("dataset loaded")
 
+sys.path.append("../src")
 from network import Network
 from layers.dense import Dense
-import numpy as np
 
 net = Network()
 
@@ -19,28 +19,8 @@ net.add_layers(
     Dense((80, 10), activation="sigmoid"),
 )
 
-# convert the mnist data so it can work in the neural net
-def vectorized_result(j):
-    e = np.zeros((10, 1))
-    e[j] = 1.0
-    return e
+net.load_model("network.json")
 
-def transform(example):
-    example["label"] = [vectorized_result(x) for x in example["label"]]
-    example["image"] = [np.array(img).reshape(784, 1) / 255 for img in example["image"]]
-    return example
-
-training_data = []
-for data in mnist["train"]:
-    training_data.append((np.array(data["image"]).reshape(784, 1) / 255, vectorized_result(data["label"])))
-
-validation_data = []
-for data in mnist["test"]:
-    validation_data.append((np.array(data["image"]).reshape(784, 1) / 255, vectorized_result(data["label"])))
-
-print("data formatted")
-
-# train the model
-net.sgd(training_data, 0.1, 32, epochs=10, validation_data=validation_data)
+net.sgd((train_data, train_ans), 0.01, epochs=10, validation_data=(test_data, test_ans))
 
 net.export_model("network.json")
