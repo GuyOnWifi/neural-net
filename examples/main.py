b64e03472ead9eb40506f1f5434a73f43e4e0168
Eason Huang <easonh887@gmail.com>
2025-01-08 15:54:22 -0500

    Normalize inputs

--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,13 @@
 import sys
 sys.path.append("../datasets")
 from dataset_loader import load, split_batches
-
+        
 data = load("mnist")
 train_data, train_ans, test_data, test_ans = data.values()
+train_data /= 255
+test_data /= 255
 train_data, train_ans = split_batches(256, train_data, train_ans)
+test_data, test_ans = split_batches(100, test_data, test_ans)
 print("dataset loaded")
 
 sys.path.append("../src")
@@ -19,8 +22,8 @@ net.add_layers(
     Dense((80, 10), activation="sigmoid"),
 )
 
-net.load_model("network.json")
+#net.load_model("network.json")
 
-net.sgd((train_data, train_ans), 0.01, epochs=10, validation_data=(test_data, test_ans))
+net.sgd((train_data, train_ans), 3, 0.01, epochs=10, validation_data=(test_data, test_ans))
 
 net.export_model("network.json")
