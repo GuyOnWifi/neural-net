65c2804925078abea60ece44a512539fbb7ca8b1
Eason Huang <easonh887@gmail.com>
2025-01-20 21:59:03 -0500

    Appeased the ts linter

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import network from "./assets/network.json";
 import * as math from "mathjs";
 import { Image } from "image-js";
